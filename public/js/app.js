cab630ea1873403495159645d975425856f1d1c2
Harsh <yharsh44@gmail.com>
2022-01-09 23:10:21 +0530

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log("js page loaded");
+// console.log("js page loaded");
 
 // fetch("http://puzzle.mead.io/puzzle").then((response) => {
 //   response.json().then((data) => {
@@ -10,26 +10,23 @@ const weatherForm = document.querySelector("form");
 
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
-const messageTwo=document.querySelector("#message-2");
+const messageTwo = document.querySelector("#message-2");
 messageOne.textContent = "";
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = search.value;
-  messageOne.textContent="loading..."
-  messageTwo.textContent=""
+  messageOne.textContent = "loading...";
+  messageTwo.textContent = "";
   fetch("http://localhost:3000/weather?address=" + location).then(
     (response) => {
       response.json().then((data) => {
-      
         if (data.error) {
-            
-          messageOne.textContent=data.error;
+          messageOne.textContent = data.error;
         } else {
-            messageOne.textContent=data.location;
-            messageTwo.textContent=data.forecast;
-        
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
         }
       });
     }
